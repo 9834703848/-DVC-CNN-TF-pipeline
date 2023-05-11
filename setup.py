d97a474ceaab6722e14f19700e209a1cb13b4429
Shrinath <maddeshrinath@gmail.com>
2023-05-11 13:21:40 +0530

    model is ready

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "DVC-CNN-TF-pipeline-demo"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "DVC-CNN-TF-pipelines"
+AUTHOR_USER_NAME = "98348703848"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = [
     "dvc",
-    "tqdm=",
+    "tqdm",
     "tensorflow",
     "joblib"
 ]
@@ -23,7 +23,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="maddeshrinath@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
